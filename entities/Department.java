75a1f70dcfe3a49dbf6151b8b6b1803eace1c512
Gilberto Felipe <gilbertofelipe.junior@gmail.com>
2024-09-20 16:06:18 -0300

    composiçoes_enumerações

---
+++ entities/Department.java
@@ -0,0 +1,26 @@
+package exercicios.enumeracoes_composicoes.entities;
+
+public class Department {
+
+    private String name;
+
+    public Department(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+        
+    
+}
+
+
+
+
---
+++ entities/enums/WorkerLevel.java
@@ -0,0 +1,9 @@
+package exercicios.enumeracoes_composicoes.entities.enums;
+
+public enum WorkerLevel {
+    
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR;
+
+}
